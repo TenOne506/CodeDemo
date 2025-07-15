5849eb852e05f8e65b42c536cd699866801ae7c7
TenThousandOne <w13852770506@163.com>
2025-07-15 23:28:46 +0800

    update kmp function

---
+++ kmp/main.cpp
@@ -0,0 +1,19 @@
+#include <cstddef>
+#include <string.h>
+#include <vector>
+
+std::vector<size_t> calculate_prefix(const std::string &s) {
+  size_t n = s.size();
+  std::vector<size_t> prefix(n, 0);
+  for (size_t i = 1; i < n; ++i) {
+    size_t j = prefix[i - 1];
+    while (j > 0 && s[i] != s[j]) {
+      j = prefix[j - 1];
+    }
+    if (s[i] == s[j]) {
+      ++j;
+    }
+    prefix[i] = j;
+  }
+  return prefix;
+}
